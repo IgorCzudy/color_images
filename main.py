c998442d84f278e58e2aaa9555aa3039f7885137
IgorCzudy <iczudy@gmail.com>
2025-03-10 15:58:29 +0100

    feat: download dataset

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from Autoencoder import Autoencoder
 from DataSetGenerator import DataSetGenerator
 from tensorflow.keras.optimizers import AdamW
-import click
+import kagglehub
+import shutil
+
+def preper_dataset():
+    path = kagglehub.dataset_download("sautkin/imagenet1k0")
+    shutil.copytree(path, ".")
+
 
 
 
 def main():
     dataSetGenerator = DataSetGenerator(batch_size=16, root_file="2")
-
     optimizer = AdamW()
     model = Autoencoder(filters=[128, 64, 32, 16], kernel_size=(3, 3))
     model.compile(optimizer=optimizer, loss='mse', metrics=['accuracy'])
